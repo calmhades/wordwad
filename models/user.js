84b45d92befd65aff47a027ae49e28f82076c0ba
Zack C <zackcozine@yahoo.com>
2018-11-02 14:18:28 -0500

    finished the overall passport structure

--- models/user.js
+++ models/user.js
@@ -1,11 +1,17 @@
-module.exports = function(sequelize, DataTypes) {
+module.exports = function (sequelize, DataTypes) {
   var User = sequelize.define("User", {
-      name: DataTypes.STRING,
-      email:{
-        type: DataTypes.STRING,
-        validate:{isEmail: true,
-                  notNull: true}
-  }
-});
+    name: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: true,
+        notNull: true
+      }
+    }
+    // need password as well
+    //password: {
+    //type: Sequelize.STRING,
+    //allowNull: false
+  });
   return User;
 };
