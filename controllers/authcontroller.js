05e7bb1abf50b8a9931acae9f52925212f656dca
Jack Morrissey <j.morrissey.16@gmail.com>
2018-11-03 14:40:45 -0500

    moved authRoute below express on server to avoid get error

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -1,17 +1,18 @@
-var exports = module.exports = {}
+module.exports = function(exports) {
 
-exports.signup = function (req, res) {
+    exports.signup = function (req, res) {
 
-    res.render('signup');
+        res.render("signup", {User: data} );
 
+    };
+
+    exports.signin = function (req, res) {
+
+        res.render("signin", {User: data} );
+
+    };
+
+    exports.dashboard = function (req, res) {
+        res.render("dashboard", {User: data});
+    };
 };
-
-exports.signin = function (req, res) {
-
-    res.render('signin');
-
-};
-
-exports.dashboard = function (req, res) {
-    res.render('dashboard');
-}
