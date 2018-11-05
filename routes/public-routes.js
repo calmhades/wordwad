115b67575bf19fedc580e10a410ffd0fcc2fb75a
Jack Morrissey <j.morrissey.16@gmail.com>
2018-11-05 17:59:21 -0600

    update

--- routes/public-routes.js
+++ routes/public-routes.js
@@ -3,11 +3,7 @@ let router = require("express").Router();
 
   // Load index page
 router.get("/", (req,res) => {
-<<<<<<< HEAD
-    res.render("index", {});
-=======
     res.render("index",{});
->>>>>>> 7663269cc8f3c4209e9c9b9d166b94e865e640ad
        
     });
   
@@ -19,20 +15,10 @@ router.get("/create", (req,res) => {
 //   })
 // }
   // Load all sotires or an individual one by ID 
-<<<<<<< HEAD
-  router.get("/story", function(req, res) {
-    res.render("browseWads", 
-      Story.findAll({})
-      .then(function(data) {
-        res.render(data);
-      })
-      );
-=======
 router.get("/story", function(req, res) {
     
         res.render("browseWads",{});
      
->>>>>>> 7663269cc8f3c4209e9c9b9d166b94e865e640ad
     });
 
 
