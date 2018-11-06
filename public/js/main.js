ed1eec090f3665605903f8107698201ede84f5e9
sahenke13 <shenke13@gmail.com>
2018-11-06 10:59:04 -0600

    working on passport

--- public/js/main.js
+++ public/js/main.js
@@ -5,31 +5,21 @@ $(document).ready(function(){
         console.log("userForm submit has been pressed");
         event.preventDefault();
       
-        var loginUser = {
-        
-          userName: $("#userName").val(),
-          password: $("#password").val(),
-        
-          
-        };
-        console.log(loginUser);
-        
-        $.post("/api/login", loginUser)
-        .then(function(data){
-          console.log(data);
-      
-        });
-      
         var userInfo = {
 
-        userName: $("#userName").val(),
-        password: $("#password").val(),
-        }
-        
-        $.get("/api/login", userInfo)
-        .then(function(data){
-          console.log(data);
-          if (userName === loginUser.userName && password === loginUser.password){
+          username: $("#userName").val(),
+          password: $("#password").val(),
+          }
+        console.log(userInfo);
+
+          
+        $.get("/login", userInfo)
+        .then(function(req, res){
+          // console.log("Data is: +++++++++++++" + data);
+          console.log("username " + userInfo.username);
+          console.log("password " + userInfo.password);
+          console.log(req.body.username)
+          if (userName === userInfo.username && password === userInfo.password){
             window.location.href="/index";
           } else {
             alert("Credentials do not match. Please try again.")
